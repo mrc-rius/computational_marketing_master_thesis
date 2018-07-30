07bf1e72977b945e9b2e99acfaeb0bf2c1fcbe83
mrius <mrius@factorenergia.com>
2018-07-30 12:47:34 +0200

    Adding real data
    1. Query is working
    2. Clustering is working
    3. Beautyfying cluster in csv -> 50%
    4. Fit predict new customer -> 0%

--- src/cluster/views.py
+++ src/cluster/views.py
@@ -7,18 +7,68 @@ from kmodes.kprototypes import KPrototypes
 from kmodes.kmodes import KModes
 from django.http import HttpResponse
 from pathlib import Path
+from django.db import connection
 
 
 # Global variables
 kproto = 0 # variable with the cluster object if it has been computed
 
-# Gets a cluster and extract all properties to create a CSV report.
 
+#Get real data from database
+def my_clustering_data():
+    with connection.cursor() as cursor:
+        #cursor.execute("SELECT foo FROM bar WHERE baz = %s", [self.baz])
+        cursor.execute("SELECT * FROM crosstab ($$ select form_token,question_id,answer_text from surveys_answer order by 1,2$$) AS final_result(submit_id varchar(200),question1 varchar(200),question2 varchar(200),question3 varchar(200),question4 varchar(200),question5 varchar(200),question6 varchar(200),question7 varchar(200),question8 varchar(200),question9 varchar(200),question10 varchar(200),question11 varchar(200),question12 varchar(200),question13 varchar(200),question14 varchar(200),question15 varchar(200),question16 varchar(200),question17 varchar(200),question18 varchar(200),question19 varchar(200),question20 varchar(200),question21 varchar(200),question22 varchar(200),question23 varchar(200),question24 varchar(200),question25 varchar(200),question26 varchar(200),question27 varchar(200))")
+        row = cursor.fetchall()
+    return row
+
+# Gets a cluster and extract all properties to create a CSV report.
 def clusterStatisticsCSV(kproto):
 
     cluster_centroids=kproto.cluster_centroids_
-    cluster_centroids_beauty=np.array([np.array([cluster_centroids[0][0][0], cluster_centroids[1][0][0], cluster_centroids[0][0][1]]),
-              np.array([cluster_centroids[0][1][0], cluster_centroids[1][1][0], cluster_centroids[0][1][1]])])
+    cluster_centroids_beauty=np.array([
+                                        np.array([
+                                                cluster_centroids[0][0][0],
+                                                cluster_centroids[1][0][0],
+                                                cluster_centroids[1][0][1],
+                                                cluster_centroids[1][0][2],
+                                                cluster_centroids[1][0][3],
+                                                cluster_centroids[1][0][4],
+                                                cluster_centroids[1][0][5],
+                                                cluster_centroids[1][0][6],
+                                                cluster_centroids[1][0][7],
+                                                cluster_centroids[1][0][8],
+                                                cluster_centroids[1][0][9],
+                                                cluster_centroids[1][0][10],
+                                                cluster_centroids[1][0][11],
+                                                cluster_centroids[1][0][12],
+                                                cluster_centroids[1][0][13],
+                                                cluster_centroids[1][0][14],
+                                                cluster_centroids[1][0][15],
+                                                cluster_centroids[1][0][16],
+                                                cluster_centroids[1][0][17],
+                                                cluster_centroids[1][0][18],
+                                                cluster_centroids[1][0][19],
+                                                cluster_centroids[1][0][20],
+                                                cluster_centroids[1][0][21],
+                                                cluster_centroids[1][0][22],
+                                                cluster_centroids[1][0][23],
+                                                cluster_centroids[1][0][24],
+                                                cluster_centroids[1][0][25],
+                                                ])
+                                        ])
+        #np.array([
+                #  np.array([
+    #                       cluster_centroids[0][0][0],
+    #                       cluster_centroids[1][0][0],
+    #                       cluster_centroids[0][0][1]
+    #             ])
+    #               ,np.array([
+    #                       cluster_centroids[0][1][0],
+    #                       cluster_centroids[1][1][0],
+    #                        cluster_centroids[0][1][1]
+    #                       ])
+    #            ])
     path = './tmp_files/cluster_results/'
     filename=datetime.datetime.now().strftime('%Y%m%d_%H%M%S')+'_kprototype_cluster_results.csv'
     with open(path+filename, 'w') as csvfile:
@@ -34,6 +84,9 @@ def clusterStatisticsCSV(kproto):
 
 def ClusterCreation(request,*args):
     global kproto
+
+    #Example of clustering with random data
+    '''
     # random categorical data
     data = np.array([
             [0,'a',4],
@@ -44,14 +97,29 @@ def ClusterCreation(request,*args):
 
     kproto = KPrototypes(n_clusters=2, init='Cao', verbose=2)
     clusters = kproto.fit(data, categorical=[1])
+    # Create CSV with cluster statistics
+    clusterStatisticsCSV(kproto)
+    for argument in args:
+        if argument is not None:
+            return
+    '''
+    # Get data from database
+    rows=my_clustering_data()
+    # Cast as numpy Array
+    rows_array=np.array(rows)
+    #Split data into variables and id's
+    data_array = np.array(rows_array)[:,1:] #dejamos sólo las variables que pueden clusterizar el cliente
+    ids_array = np.array(rows_array)[:, 0] #guardamos las id's en otro array
 
+    #Clustering
+    kproto = KPrototypes(n_clusters=2, init='Cao', verbose=2)
+    clusters = kproto.fit(data_array, categorical=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26])
     # Create CSV with cluster statistics
     clusterStatisticsCSV(kproto)
     for argument in args:
         if argument is not None:
             return
 
-    # Print the cluster centroids
     return HttpResponse('Clustering realizado y CSV report generado')
 
 def ClusterPrediction(request):
--- src/tmp_files/sql/form_query_pivot.sql
+++ src/tmp_files/sql/form_query_pivot.sql
@@ -1,5 +1,4 @@
 
-'''
 CREATE extension tablefunc;
 
 SELECT *
@@ -11,32 +10,34 @@ FROM crosstab (
      AS
      final_result(
       submit_id  varchar(200),
-      "question1" varchar(200),
-       "question2" varchar(200),
-       "question3" varchar(200),
-       "question4" varchar(200),
-       "question5" varchar(200),
-       "question6" varchar(200),
-       "question7" varchar(200),
-       "question8" varchar(200),
-       "question9" varchar(200),
-       "question10" varchar(200),
-       "question11" varchar(200),
-       "question12" varchar(200),
-       "question13" varchar(200),
-       "question14" varchar(200),
-       "question15" varchar(200),
-       "question16" varchar(200),
-       "question17" varchar(200),
-       "question18" varchar(200),
-       "question19" varchar(200),
-       "question20" varchar(200),
-       "question21" varchar(200),
-      "question22" varchar(200),
-      "question23" varchar(200),
-      "question24" varchar(200),
-      "question25" varchar(200),
-      "question26" varchar(200),
-      "question27" varchar(200)
+      question1 varchar(200),
+      question2 varchar(200),
+      question3 varchar(200),
+      question4 varchar(200),
+      question5 varchar(200),
+      question6 varchar(200),
+      question7 varchar(200),
+      question8 varchar(200),
+      question9 varchar(200),
+      question10 varchar(200),
+      question11 varchar(200),
+      question12 varchar(200),
+      question13 varchar(200),
+      question14 varchar(200),
+      question15 varchar(200),
+      question16 varchar(200),
+      question17 varchar(200),
+      question18 varchar(200),
+      question19 varchar(200),
+      question20 varchar(200),
+      question21 varchar(200),
+      question22 varchar(200),
+      question23 varchar(200),
+      question24 varchar(200),
+      question25 varchar(200),
+      question26 varchar(200),
+      question27 varchar(200)
      );
-'''
+
+
+SELECT * FROM crosstab ($$ select form_token,question_id,answer_text from surveys_answer order by 1,2$$) AS final_result(submit_id varchar(200),question1 varchar(200),question2 varchar(200),question3 varchar(200),question4 varchar(200),question5 varchar(200),question6 varchar(200),question7 varchar(200),question8 varchar(200),question9 varchar(200),question10 varchar(200),question11 varchar(200),question12 varchar(200),question13 varchar(200),question14 varchar(200),question15 varchar(200),question16 varchar(200),question17 varchar(200),question18 varchar(200),question19 varchar(200),question20 varchar(200),question21 varchar(200),question22 varchar(200),question23 varchar(200),question24 varchar(200),question25 varchar(200),question26 varchar(200),question27 varchar(200))

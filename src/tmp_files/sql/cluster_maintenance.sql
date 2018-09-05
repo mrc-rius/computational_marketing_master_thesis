af429a8b9d16e75e74cc7e369fc7407cc1dc1777
mrius <mrius@factorenergia.com>
2018-09-05 09:20:39 +0200

    Last mods in sql data
    Add some mods in insert statements for some base data.

--- src/tmp_files/sql/cluster_maintenance.sql
+++ src/tmp_files/sql/cluster_maintenance.sql
@@ -7,5 +7,10 @@ INSERT INTO cluster_maintenance(maintenance_name, maintenance_price, maintenance
 INSERT INTO cluster_maintenance(maintenance_name, maintenance_price, maintenance_level,maintenance_customer_type,maintenance_tariff, deleted) VALUES ('Factor Luz',8.89,2,'Domestico','2.1 DHS',0);
 INSERT INTO cluster_maintenance(maintenance_name, maintenance_price, maintenance_level,maintenance_customer_type,maintenance_tariff, deleted) VALUES ('Hogar Confort',14.17,4,'Domestico','3.0 A',0);
 INSERT INTO cluster_maintenance(maintenance_name, maintenance_price, maintenance_level,maintenance_customer_type,maintenance_tariff, deleted) VALUES ('Hogar Premium',18.53,4,'Domestico','3.1 A',0);
-INSERT INTO cluster_maintenance(maintenance_name, maintenance_price, maintenance_level,maintenance_customer_type,maintenance_tariff, deleted) VALUES ('Premium',40.89,5,'Domestico','6.1 A',0);
-INSERT INTO cluster_maintenance(maintenance_name, maintenance_price, maintenance_level,maintenance_customer_type,maintenance_tariff, deleted) VALUES ('Premium',50.24,5,'Domestico','X',0);
+INSERT INTO cluster_maintenance(maintenance_name, maintenance_price, maintenance_level,maintenance_customer_type,maintenance_tariff, deleted) VALUES ('Factor Luz',8.89,2,'Empresa','2.1 A',0);
+INSERT INTO cluster_maintenance(maintenance_name, maintenance_price, maintenance_level,maintenance_customer_type,maintenance_tariff, deleted) VALUES ('Factor Luz',8.89,2,'Empresa','2.1 DHA',0);
+INSERT INTO cluster_maintenance(maintenance_name, maintenance_price, maintenance_level,maintenance_customer_type,maintenance_tariff, deleted) VALUES ('Factor Luz',8.89,2,'Empresa','2.1 DHS',0);
+INSERT INTO cluster_maintenance(maintenance_name, maintenance_price, maintenance_level,maintenance_customer_type,maintenance_tariff, deleted) VALUES ('Hogar Confort',14.17,4,'Empresa','3.0 A',0);
+INSERT INTO cluster_maintenance(maintenance_name, maintenance_price, maintenance_level,maintenance_customer_type,maintenance_tariff, deleted) VALUES ('Hogar Premium',18.53,4,'Empresa','3.1 A',0);
+INSERT INTO cluster_maintenance(maintenance_name, maintenance_price, maintenance_level,maintenance_customer_type,maintenance_tariff, deleted) VALUES ('Premium',40.89,5,'Empresa','6.1 A',0);
+INSERT INTO cluster_maintenance(maintenance_name, maintenance_price, maintenance_level,maintenance_customer_type,maintenance_tariff, deleted) VALUES ('Premium',50.24,5,'Empresa','X',0);
